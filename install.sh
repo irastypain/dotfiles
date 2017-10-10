ac03a77177778cee37ec6fa71c0dd5b1da615982
Ivan Rastyapin <i.rastypain@gmail.com>
2017-10-10 16:37:22 +0300

    Added configuring .gitconfig

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# .Xresouces
 cp -f ./.Xresources ~/.Xresources && \
-
 mkdir -p ~/.config/.Xresources.d && \
-
 cp -Rf ./.config/.Xresources.d/* ~/.config/.Xresources.d && \
-
 xrdb -load ~/.Xresources
+
+# .gitconfig
+cp -f ./files/gitconfig ~/.gitconfig
