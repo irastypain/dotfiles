3b38275c916d982ea34882cbe83eea1f412d474b
Ivan Rastyapin <i.rastypain@gmail.com>
2017-10-10 17:05:24 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # .Xresources
 cp -f ./files/Xresources ~/.Xresources && \
 mkdir -p ~/.config/Xresources.d && \
-cp -Rf ./files/config/Xresources.d/* ~/.config/.Xresources.d && \
+cp -Rf ./files/config/Xresources.d/* ~/.config/Xresources.d && \
 xrdb -load ~/.Xresources
 
 # .gitconfig
