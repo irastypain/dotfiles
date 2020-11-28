6f25e47a39b54572837e557e669e23ecdcd57cf0
Ivan Rastyapin <i.rastypain@gmail.com>
2020-11-28 04:13:57 +0300

    Remove redundant code from git configurators

--- configurators/git.sh
+++ configurators/git.sh
@@ -3,12 +3,3 @@
 # Copy general git config
 cp -i files/gitignore_global "$HOME/.gitignore_global"
 cp -i files/gitconfig "$HOME/.gitconfig"
-
-# Create project directories with empty .gitconfig
-ProjectDirs=('home' 'oss' 'rubycrutch' 'work')
-
-for dir in "${ProjectDirs[@]}"; do
-  dir_path="$HOME/projects/test/$dir"
-  mkdir -p $dir_path
-  touch "$dir_path/.gitconfig"
-done
